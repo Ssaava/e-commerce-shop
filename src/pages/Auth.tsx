0d0385550d01c623dcc644fa6437d270617265fb
frankamale <108013583+frankamale@users.noreply.github.com>
2024-08-29 01:21:54 +0300

    edited auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,13 +1,9 @@
 import { Link } from "react-router-dom";
 
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
