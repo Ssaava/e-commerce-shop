98e36be9dd9866ba5a8b940b7af193782476c0eb
frankamale <108013583+frankamale@users.noreply.github.com>
2024-08-29 14:48:11 +0300

    Added a contact us page

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,9 +1,23 @@
-import { Link } from "react-router-dom"
+import AssistCard from "@/components/AssistCard";
+import ContactCard from "@/components/ContactCard";
+import { Card } from "@/components/ui/card";
+import { Link } from "react-router-dom";
 
+const topics = [
+  "How do I return my item?",
+  "How long is the refund process?",
+  "What is Misercom Returns Policy?",
+  "What are the 'Delivery Timelines'?",
+  "How to cancel Misercom Order.",
+  "What is the Voucher & Gift Offer in this Campaign?",
+  "What is 'Discover Your Daraz Campaign 2022'?",
+  "Ask the Digital and Device Community",
+  "How to change my shop name?",
+];
 const ContactUs = () => {
   return (
     <>
-    <div className={"section py-5 bg-gray-50"}>
+      <div className={"section py-5 bg-gray-50"}>
         <ul className={"flex gap-4 items-center text-gray-600 text-sm"}>
           <Link
             to={"/"}
@@ -16,14 +30,132 @@ const ContactUs = () => {
               Home <i className="ml-2 fa-solid fa-angle-right"></i>
             </li>
           </Link>
-         
+
           <Link to={"/products"}>
             <li className=" text-secondary-500">Customer Support</li>
           </Link>
         </ul>
       </div>
-    </>
-  )
-}
 
-export default ContactUs
+      <section className="section flex py-10 justify-between items-center">
+        <div>
+          <Card className="bg-warning-400 w-fit py-1 px-3 rounded-none text-sm font-semibold">
+            HELP CENTER
+          </Card>
+          <p className="text-gray-900 text-[28px]">How can we help you!</p>
+          <div className="flex justify-center bg-white px-3 py-2 my-5 border items-center w-[30vw]">
+            <i className="fa-solid fa-magnifying-glass text-primary-500"></i>
+            <input
+              type="email"
+              placeholder="Enter your question of keyword"
+              className="w-full h-[40px] px-5 "
+            />
+            <button
+              type="submit"
+              className="bg-primary-500 hover:bg-primary-700 text-white flex justify-center items-center gap-2 px-5 py-1"
+            >
+              search
+            </button>
+          </div>
+        </div>
+        <img src="/images/businesswoman.png" alt="business woman" width={400} />
+      </section>
+
+      <section className="section">
+        <p className="text-gray-900 text-[28px] text-center">
+          How can we assist you today?{" "}
+        </p>
+
+        <div className="grid grid-cols-4 gap-3 py-7 ">
+          <AssistCard icon="fa-solid fa-truck" name="Track order" link="/" />
+          <AssistCard
+            name="Reset Password"
+            icon="fa-solid fa-unlock"
+            link="/"
+          />
+          <AssistCard
+            name="Payment option"
+            icon="fa-regular fa-credit-card"
+            link="/"
+          />
+          <AssistCard name="User Account" icon="fa-regular fa-user" link="/" />
+          <AssistCard
+            name="Wishlist & compare"
+            icon="fa-solid fa-layer-group"
+            link="/"
+          />
+          <AssistCard
+            name="Shipping & billing"
+            icon="fa-solid fa-receipt"
+            link="/"
+          />
+          <AssistCard
+            name="Shopping cart & wallet"
+            icon="fa-regular fa-credit card"
+            link="/"
+          />
+          <AssistCard
+            name="Sell on Misercom"
+            icon="fa-solid fa-shop"
+            link="/"
+          />
+        </div>
+      </section>
+      <section className="section">
+        <p className="text-gray-900 font-bold text-[20px] text-center pt-5">
+          Popular topics
+        </p>
+        <div className="grid grid-cols-3 gap-3 py-7 text-sm">
+          {topics.map((topic, index) => (
+            <Link
+              key={index}
+              to={"#"}
+              className="hover:text-primary-500 transition duration-150"
+            >
+              <li>{topic}</li>
+            </Link>
+          ))}
+        </div>
+      </section>
+
+      <section className="bg-gray-50 py-10">
+        <div className="section ">
+          <div className="center-item flex-col">
+            <button
+              type="submit"
+              className="bg-secondary-500 hover:bg-primary-700 text-white flex justify-center items-center gap-2 px-5 py-1"
+            >
+              CONTACT US
+            </button>
+            <p className="text-gray-900 text-[28px] text-center mb-[-10px] mt-5 font-semibold">
+              Didn't find the answer,
+            </p>
+            <p className="text-gray-900 text-[28px] text-center font-semibold">
+              Contact with us
+            </p>
+          </div>
+
+          <div className="flex gap-5">
+            <ContactCard
+              image="/images/call.png"
+              title="Call us now"
+              info="we are available online from 9:00 AM to 5:00 PM (GMT95:45) Talk with use now"
+              contact="+1-202-555-0126"
+              button="CALL NOW"
+              color="bg-secondary-500"
+            />
+            <ContactCard
+              image="/images/message.png"
+              title="Chat with us"
+              info="we are available online from 9:00 AM to 5:00 PM (GMT95:45) Talk with use now"
+              contact="support@misercom.com"
+              button="CONTACT US"
+              color="bg-success-500"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default ContactUs;
